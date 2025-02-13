568be7c90140abc4c9b2c48ed59c3e7e3b4bd20b
sidharthgk <sidharthgk2003@gmail.com>
2025-02-13 14:47:52 +0530

    added loadingscreen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,6 +15,7 @@ const Navbar = () => {
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
+    
   }, []);
 
   const isActive = (path: string) => location.pathname === path;
